1d8c0e4d79fe4acc642dd86344b4ecf96731ca99
Darwin Granda <darwin.granda@unl.edu.ec>
2024-05-21 18:23:55 -0500

    Proyecto base

---
+++ PIS/routes/router.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify, abort , request, render_template, redirect, make_response
+from flask import flash
+from flask_cors import CORS
+import time
+router = Blueprint('router', __name__)
+
+
+
+
+#CORS(api)
+cors = CORS(router, resource={
+    r"/*":{
+        "origins":"*"
+    }
+})
+
+#GET: PARA PRESENTAR DATOS
+#POST: GUARDA DATOS, MODIFICA DATOS Y EL INICIO DE SESION, EVIAR DATOS AL SERVIDOR
+
+@router.route('/') #SON GETS
+def home():
+    return render_template('inicio.html')
+
+
