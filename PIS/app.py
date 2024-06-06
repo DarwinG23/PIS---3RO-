27fb8ac0a28b67171445827e975e73cf3bce6c8b
Darwin Granda <darwin.granda@unl.edu.ec>
2024-06-05 22:05:57 -0500

    bug de librerias

--- PIS/app.py
+++ PIS/app.py
@@ -1,7 +1,7 @@
-from flask import flask
+from flask import Flask
 
 def create_app():
-    app = flask(__name__, instance_relative_config=False)
+    app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.config.Config')
     with app.app_context():
         #from routes.api import api
